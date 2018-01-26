2dc0376a534fa8fc083078637c2f24952eaab8a4
fuhria <paul.lefebvre777@gmail.com>
2018-01-27 00:17:01 +0100

    GameManager added

---
+++ Assets/Scripts/Call.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Call : MonoBehaviour
+{
+    float randomCountDown = Random.Range(0, 60);
+
+    // Use this for initialization
+    void Start ()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+    }
+
+    public float Timers()
+    {
+        randomCountDown -= Time.deltaTime;
+
+        return randomCountDown;
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private int score = 0;
+    private int lives = 3;
+
+    [SerializeField]
+    Color[] colorArray;
+
+    [SerializeField]
+    List<GameObject> availableHosts;
+
+    private List<GameObject> unavailableHosts;
+
+    private int[] id;
+
+    private void Start()
+    {
+        Call();
+    }
+
+    private void Call()
+    {
+
+        if (availableHosts.Count > 2)
+        {
+            int randomCaller = Random.Range(0, availableHosts.Count);
+            int randomReciever = Random.Range(0, availableHosts.Count);
+
+            while(randomCaller == randomReciever)
+            {
+                randomReciever = Random.Range(0, availableHosts.Count);
+            }
+
+
+            GameObject reciever = availableHosts[randomReciever];
+            availableHosts.Remove(reciever);
+            unavailableHosts.Add(reciever);
+
+            GameObject caller = availableHosts[randomCaller];
+            availableHosts.Remove(caller);
+            unavailableHosts.Add(caller);
+        }
+    }
+
+    private void EndCall(bool success)
+    {
+        if(success)
+        {
+            score++;
+        }
+        else
+        {
+            lives--;
+        }      
+    }
+}
