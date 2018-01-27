f924747b6feb519ecc454ed90e15ea239c290a21
brandy12 <brand_1995@hotmail.com>
2018-01-27 05:58:56 +0100

    ajout : curseur, couleur des edges, chronos, liaisons des nodes, chemin, gestion score et vie

--- Assets/Scripts/Call.cs
+++ Assets/Scripts/Call.cs
@@ -2,26 +2,71 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Call : MonoBehaviour
+public class Call
 {
-    float randomCountDown = Random.Range(0, 60);
+    private static int ID=0;
+    public int id{get;private set;}
 
-    // Use this for initialization
-    void Start ()
-    {
+    public Call(){
+        id = ID;
+        ID++;
 
-	}
-	
-	// Update is called once per frame
-	void Update ()
-    {
-
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        Debug.Log("Create call");
     }
+
+    //premier chrono en seconde,
+    //temps avant que la personne abandonne l'appel (-1 vie)
+    float randomCountDown = Random.Range(10, 20);
+
+    public NodeController caller{get;set;}
+    public NodeController reciever { get; set; }
+
+    public enum Status{calling, inCall};
+    public Status status{get;set;}
+
+    GameManager gameManager;
 
     public float Timers()
     {
         randomCountDown -= Time.deltaTime;
 
         return randomCountDown;
+    }
+
+    public void SetInCall()
+    {
+        status = Status.inCall;
+
+        //second chrono en seconde,
+        //temps avant que la communication s'achève
+        randomCountDown = Random.Range(10, 20);
+    }
+
+    public bool Update()
+    {
+        if(randomCountDown<0)
+        {
+            if(status == Status.inCall)
+            {
+                gameManager.Liberer(caller);
+                gameManager.Liberer(reciever);
+                gameManager.EndCall(true);
+                return true;
+            } 
+            else if (status == Status.calling)
+            {
+                gameManager.Liberer(caller);
+                gameManager.Liberer(reciever);
+                gameManager.EndCall(false);
+                return true;
+            }
+        }
+        else
+        {
+            randomCountDown -= Time.deltaTime;
+            Debug.Log(randomCountDown);
+        }
+        return false;
     }
 }
--- Assets/Scripts/NodeController.cs
+++ Assets/Scripts/NodeController.cs
@@ -3,27 +3,64 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class NodeController : MonoBehaviour {
+    
+    const double limit_radius = 0.5;//distance à partir de laquelle, la souris "touche" un node
 
-    [SerializeField] List<EdgeController> neightborhoodList;
+    [SerializeField] public List<EdgeController> neighborhoodList;
     [SerializeField] GameObject cursorPrefab;
 
     [SerializeField] private bool isHost;
 
+    public enum Status{ idle, calling, waitingCall, inCall};
+    public Status status=Status.idle;
+
+    public Call call{get;set;}//référence à l'appel associé au host
+
     private SpriteRenderer render;
+    private GameManager gameManager;
 
     private void Start()
     {
         render = GetComponent<SpriteRenderer>();
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+
+    private void Update()
+    {
+    //ajouter la node parcequ'on est proche
+        if((Camera.main.ScreenToWorldPoint(Input.mousePosition+new Vector3(0,0,10))-transform.position).magnitude<limit_radius && !isHost){//si suffisamment proche
+            gameManager.AddNodeToTrajectory(this);
+        }
+
+
+        //mouse down
+        if(Input.GetMouseButtonDown(0) && (Camera.main.ScreenToWorldPoint(Input.mousePosition+new Vector3(0,0,10))-transform.position).magnitude<limit_radius){
+            if(isHost && status==Status.calling) {
+                /*GameObject iCursor = Instantiate(cursorPrefab, cursorPrefab.transform.position, cursorPrefab.transform.rotation);
+                iCursor.GetComponent<CursorBehavior>().SetCurrentNode(this);*/
+
+                gameManager.BeginTrajectory(this);
+            } 
+        }
+        
+        //mouse up
+        if(Input.GetMouseButtonUp(0) && (Camera.main.ScreenToWorldPoint(Input.mousePosition+new Vector3(0,0,10))-transform.position).magnitude<limit_radius) {
+            if(isHost && status==Status.waitingCall){
+                if(gameManager.trajectory().Count>0 && gameManager.trajectory()[0].call.reciever == this){
+                    gameManager.EndTrajectory(this);
+                }
+            }
+        }
     }
 
     public void AddEdge(EdgeController edge)
     {
-        neightborhoodList.Add(edge);
+        neighborhoodList.Add(edge);
     }
 
     public bool IsConnectedTo(NodeController otherNode)
     {
-        foreach(EdgeController edge in neightborhoodList)
+        foreach(EdgeController edge in neighborhoodList)
         {
             if(edge.KnowOtherSide(this) == otherNode)
             {
@@ -34,17 +71,24 @@ public class NodeController : MonoBehaviour {
         return false;
     }
 
+    public EdgeController edge(NodeController otherNode)
+    {
+        //renvoie le edge qui lie this à otherNode
+        foreach(EdgeController e in neighborhoodList)
+        {
+            if(e.KnowOtherSide(this) == otherNode)
+            {
+                return e;
+            }
+        }
+
+        return null;
+    }
+
     public void ChangeColor(Color newColor)
     {
         render.color = newColor;
+        
     }
-
-    private void OnMouseDown()
-    {
-        if (isHost)
-        {
-            GameObject iCursor = Instantiate(cursorPrefab, cursorPrefab.transform.position, cursorPrefab.transform.rotation);
-            iCursor.GetComponent<CursorBehavior>().SetCurrentNode(this);
-        }
-    }
+   
 }
